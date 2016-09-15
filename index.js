e6fb1ef37488bc499ede1850d1c3f403dacfcc06
George Wu <uiharu@buaa.edu.cn>
2016-09-15 12:10:06 +0200

    Prepare to be exported and compiled.

--- index.js
+++ index.js
@@ -2,17 +2,16 @@
  * Created by MIC on 2016/9/15.
  */
 
-(function () {
-    var testScripts = [
-        {"name": "Blank", "desc": "Blank", file: ""},
-        {"name": "3D-Ball", "desc": "3D ball", "file": "3d-ball.as"},
-        {"name": "GreenDam", "desc": "Green Dam Musume", "file": "kanpai-green-dam.as"},
-        {"name": "Madoka", "desc": "Madoka and her happy <del>tree</del> friends", "file": "kanpai-madoka.as"}
-    ];
-
-    return testScripts;
-})();
+var testScripts = [
+    {"name": "Blank", "desc": "Blank", file: ""},
+    {"name": "3D-Ball", "desc": "3D ball", "file": "3d-ball.as"},
+    {"name": "GreenDam", "desc": "Green Dam Musume", "file": "kanpai-green-dam.as"},
+    {"name": "Madoka", "desc": "Madoka and her happy <del>tree</del> friends", "file": "kanpai-madoka.as"}
+];
 
 if (module && typeof module === "object") {
     module.exports = testScripts;
+} else {
+    // So, no strict mode because we are going to eval this function.
+    return testScripts;
 }
