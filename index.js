065313726b6f708240498d9d70269508f55f4de8
George Wu <uiharu@buaa.edu.cn>
2016-09-15 11:55:18 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by MIC on 2016/9/15.
+ */
+
+(function () {
+    var testScripts = [
+        {"name": "Blank", "desc": "Blank", file: ""},
+        {"name": "3D-Ball", "desc": "3D ball", "file": "3d-ball.as"},
+        {"name": "GreenDam", "desc": "Green Dam Musume", "file": "kanpai-green-dam.as"},
+        {"name": "Madoka", "desc": "Madoka and her happy <del>tree</del> friends", "file": "kanpai-madoka.as"}
+    ];
+
+    if (module && typeof module === "object") {
+        module.exports = testScripts;
+    }
+    return testScripts;
+})();
